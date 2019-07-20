fa32bdbfd3247f30850419b4019d928aae3a4468
wvanlief <wvanliefferinge@gmail.com>
2019-07-20 16:13:28 +0200

    add abstract class lecture

---
+++ lectures/AnonymousClasses.scala
@@ -0,0 +1,32 @@
+package lectures
+
+object AnonymousClasses extends App {
+
+  abstract class Animal {
+    def eat: Unit
+
+  }
+
+  val funnyAnimal: Animal = new Animal { //anonymous class
+    override def eat: Unit = println("ahahahahahah")
+  } // we can instantiate types and override fields or methods on the spot
+    // here we instantiate a new animal and override the abstract eat function
+  /*
+  equivalent with
+
+  class AnonymousClasses$$anon$1 extends Animal {
+      override def eat: Unit = println("ahahahahhah")
+      }
+      val funnyAnimal: Animal = new AnonymousClasses$$anon$1
+   */
+
+  println(funnyAnimal.getClass)
+
+  class Person(name: String) {
+    def sayHi: Unit = print(s"Hi, my name is $name, how can i help ?")
+  }
+
+  val jim = new Person("Jim") {
+    override def sayHi: Unit = println(s"Hi, my name is Jim, how can i be of service?")
+  }
+}
